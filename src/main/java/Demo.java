415413c347e1213abb6495922d1a85a5fdc292d6
Artem Labazov <123321artyom@gmail.com>
2021-02-23 21:49:20 +0300

    Initial bare-bones frame

---
+++ src/main/java/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        Demo exampleChart = new Demo();
+        exampleChart.run();
+    }
+
+    private void run() {
+        final SwingWrapper swingWrapper = new SwingWrapper().setTitle("ok retard");
+        swingWrapper.displayChart();
+    }
+}
