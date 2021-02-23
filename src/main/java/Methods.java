bf69c67402e77667163b98826ba8c99a4da5e0e1
flydzen <flydzen@gmail.com>
2021-02-24 01:09:58 +0300

    Add dichotomy method, fix golderRatio, add main in Methods for tests in place

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -3,22 +3,47 @@ public class Methods {
     public static Result goldenRatio(AbstractFunction fun, double from, double to, double epsLim) {
         final Result res = new Result();
         final double Tau = (Math.sqrt(5) - 1) / 2;
-        double[] x = new double[]{from + (1 - Tau) * (to - from), from + Tau * (to - from)};
+        double I = (to - from) * Tau;
+        double[] x = new double[]{to - I, from + I};
         double[] f = new double[]{fun.eval(x[0]), fun.eval(x[1])};
         double[] ab = new double[]{from, to};
         for (double eps = (ab[1] - ab[0]) / 2; eps > epsLim; eps *= Tau) {
-            double b = ab[1];
+            I *= Tau;
             int le = f[0] <= f[1] ? 1 : 0;
             ab[le] = x[le];
             x[le] = x[1 - le];
-            x[1 - le] = b - Tau * (b - ab[0]);
-            f[le] = f[le - 1];
+            x[1 - le] = ab[le] + (le == 0 ? I : -I);
+            f[le] = f[1 - le];
             f[1 - le] = fun.eval(x[1 - le]);
-            eps *= Tau;
             res.addStep(x[0], x[1], f[0], f[1]);
         }
-        double x_ = (x[0] + x[1]) / 2;
+        double x_ = (ab[0] + ab[1]) / 2;
         res.setResult(x_, fun.eval(x_));
         return res;
+    }
+
+    public static Result dichotomy(AbstractFunction fun, double from, double to, double epsLim) {
+        final Result res = new Result();
+        double d = (to-from)/3;
+        double[] x = new double[]{(from + to - d) / 2, (from + to + d) / 2};
+        double[] f;
+        double[] ab = new double[]{from, to};
+        res.addStep(x[0], x[1], fun.eval(x[0]), fun.eval(x[1]));
+        for (double eps = (ab[1] - ab[0]) / 2; eps > epsLim; eps = (ab[1] - ab[0]) / 2) {
+            d = (ab[1]-ab[0])/3;
+            x = new double[]{(ab[0] + ab[1] - d) / 2, (ab[0] + ab[1] + d) / 2};
+            f = new double[]{fun.eval(x[0]), fun.eval(x[1])};
+            int le = f[0] <= f[1] ? 1 : 0;
+            ab[le] = x[le];
+            res.addStep(x[0], x[1], f[0], f[1]);
+        }
+        double x_ = (ab[0] + ab[1]) / 2;
+        res.setResult(x_, fun.eval(x_));
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Methods.goldenRatio(new FunVar2(), -1, 1, 1e-15));
+        System.out.println(Methods.dichotomy(new FunVar2(), -1, 1, 1e-15));
     }
 }
