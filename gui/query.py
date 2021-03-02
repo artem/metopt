7bcf7081377c9c4770787ea71d20c07c67b9b178
Artem Labazov <123321artyom@gmail.com>
2021-03-03 01:22:03 +0300

    Limit memory usage to 512MB

--- gui/query.py
+++ gui/query.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def call(number, eps):
     # C:\\Program Files\\Java\\jdk-14.0.2\\bin\\
-    inp = subprocess.check_output(["java", "-jar",
+    inp = subprocess.check_output(["java", "-Xmx512M", "-jar",
                                    values.jar_path, str(number), str(eps)])
     data = inp.splitlines()
     return float(data[0]), float(data[1]), [list(map(float, i.split())) for i in data[2:]]
