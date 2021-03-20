f872054ca7d2b56372a9afce02cf3df043bab261
Artem Labazov <123321artyom@gmail.com>
2021-03-20 20:25:37 +0300

    Memorize values in fibonacci method

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -2,21 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Fibonacci {
-    private static final ArrayList<Double> fib = new ArrayList<>(List.of(1.0, 1.0));
+    private static final ArrayList<Double> fib = new ArrayList<>(List.of(0.0, 1.0, 1.0));
 
     public static double get(final int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("n must be positive");
+        if (n < 0) {
+            throw new IllegalArgumentException("n must be non-negative");
         }
 
-        if (n > fib.size()) {
-            fib.ensureCapacity(n);
-            for (int i = fib.size() + 1; i <= n; i++) {
-                fib.add(fib.get(i - 3) + fib.get(i - 2));
+        if (n > fib.size() - 1) {
+            fib.ensureCapacity(n + 1);
+            for (int i = fib.size(); i <= n; i++) {
+                fib.add(fib.get(i - 2) + fib.get(i - 1));
             }
         }
 
-        return fib.get(n - 1);
+        return fib.get(n);
     }
 
     public static double relation(int a, int b) {
