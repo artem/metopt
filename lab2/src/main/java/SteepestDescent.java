0ea935bc7e2269ada33e0d503414d348e546f42e
Ivan Naumov <ivanln7514@gmail.com>
2021-04-14 01:36:15 +0300

    SteepestDescent upper bound for alpha and max_iterations number added

--- lab2/src/main/java/SteepestDescent.java
+++ lab2/src/main/java/SteepestDescent.java
@@ -1,12 +1,25 @@
 import java.util.function.UnaryOperator;
 
 public class SteepestDescent {
+    private static final int MAX_ITERATIONS = 3000;
+
     private final AbstractFunction f;
     private final double eps;
 
     public SteepestDescent(final AbstractFunction f, final double eps) {
         this.f = f;
         this.eps = eps;
+    }
+
+    double maxFactor(Matrix factor, Matrix product) {
+        double result = 0;
+        for (int i = 0; i < product.m; ++i) {
+            double f1 = Math.abs(product.get(i, 0) / factor.get(i, 0));
+            if (f1 > result && !Double.isInfinite(f1)) {
+                result = f1;
+            }
+        }
+        return result;
     }
 
     public Matrix process() throws MatrixException {
@@ -16,17 +29,22 @@ public class SteepestDescent {
         Matrix p = f.gradient(x).invert();
         result.add(x);
 
-        while (p.len() > eps) {
+        for (int i = 0; i < MAX_ITERATIONS; ++i) {
+            if (p.len() < eps) {
+                return x;
+            }
             Matrix finalX = x;
             Matrix finalP = p;
-            UnaryOperator<Double> g1 = z -> f.eval(Matrix.sum(finalX, Matrix.mul(finalP, z)));
-            double a = SingleDimensionMethods.parabola(g1, eps, 0, 1000000); // TODO
+            UnaryOperator<Double> g = z -> f.eval(Matrix.sum(finalX, Matrix.mul(finalP, z)));
+            double aUpperBound = Math.max(maxFactor(p, Matrix.sum(f.start, x.invert())),
+                                          maxFactor(p, Matrix.sum(f.end, x.invert())));
+            double a = SingleDimensionMethods.fib(g, eps, 0, aUpperBound); // TODO
             x = Matrix.sum(x, Matrix.mul(p, a));
             p = f.gradient(x).invert();
             result.add(x);
         }
-        System.out.println("steepest descent took " + result.getSteps().size() + " iterations.");
-        return x;
+
+        throw new AssertionError("Processing timeout");
     }
 
     public static void main(String[] args)  {
