5bc4dbed5e1f65a5e2a4b5b5b7b818b3ffab0ce7
Artem Labazov <123321artyom@gmail.com>
2021-04-13 23:18:40 +0300

    Gradient desc tracing

--- lab2/src/main/java/GradientDescent.java
+++ lab2/src/main/java/GradientDescent.java
@@ -1,38 +1,46 @@
 import static java.lang.Double.NaN;
 
 public class GradientDescent {
+    private final AbstractFunction fn;
+    private final double gradEps;
 
-    AbstractFunction fn;
-    double gradEps;
-
-    public GradientDescent(AbstractFunction fn, double gradEps) {
+    public GradientDescent(final AbstractFunction fn, final double gradEps) {
         this.fn = fn;
         this.gradEps = gradEps;
     }
 
     // не тестировал
-    public double process(double lambda, boolean normalGrad) throws MatrixException {
+    public Trace process(double lambda, final boolean normalGrad) throws MatrixException {
+        final Trace result = new Trace(fn);
+
         Matrix x = Matrix.mul(Matrix.sum(fn.start, fn.end), .5);
         double fx = fn.eval(x);
+
+        result.add(x);
+
         for (int i = 0; i < 3000; i++) {
             Matrix gradient = fn.gradient(x);
             if (gradient.len() < gradEps) {
-                return fx;
+                return result;
             }
-            if (normalGrad) // замечание к алгоритму, как возможный вариант.
-                gradient = Matrix.mul(gradient, 1/gradient.len());
+            if (normalGrad) { // замечание к алгоритму, как возможный вариант.
+                gradient = Matrix.mul(gradient, 1 / gradient.len());
+            }
+
             while (true) {
-                Matrix y = Matrix.sum(x, Matrix.mul(gradient, -lambda));
+                final Matrix y = Matrix.sum(x, Matrix.mul(gradient, -lambda));
                 double fy = fn.eval(y);
                 if (fy < fx) {
                     x = y;
                     fx = fy;
+                    result.add(x);
                     break;
                 } else {
                     lambda /= 2;
                 }
             }
         }
-        return NaN;
+
+        throw new AssertionError("Processing timeout");
     }
 }
--- lab2/src/main/java/Trace.java
+++ lab2/src/main/java/Trace.java
@@ -1,15 +1,31 @@
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Trace {
     private final AbstractFunction function;
+    private final ArrayList<Matrix> steps = new ArrayList<>();
 
     public Trace(AbstractFunction fun) {
         this.function = fun;
+    }
+
+    public void add(Matrix x) {
+        steps.add(x);
     }
 
     @Override
     public String toString() {
         final Gson gson = new Gson();
         return gson.toJson(this);
+    }
+
+    public AbstractFunction getFunction() {
+        return function;
+    }
+
+    public List<Matrix> getSteps() {
+        return steps;
     }
 }
