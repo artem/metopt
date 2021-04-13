b1dc218216df4f27032c092dad2862760270fd42
flydzen <flydzen@gmail.com>
2021-04-13 19:37:40 +0300

    fix function1

--- lab2/src/main/java/Function1.java
+++ lab2/src/main/java/Function1.java
@@ -5,7 +5,7 @@ public class Function1 extends AbstractFunction {
     public Function1() {
         this.start = new Matrix(List.of(-100., -100.), true);
         this.end = new Matrix(List.of(100., 100.), true);
-        this.A = new Matrix(List.of(List.of(54.,126.),List.of(126.,64.)));
+        this.A = new Matrix(List.of(List.of(2*54.,126.),List.of(126.,2*64.)));
         this.b = new Matrix(List.of(-10., 30.), true);
         this.c = 13;
     }
