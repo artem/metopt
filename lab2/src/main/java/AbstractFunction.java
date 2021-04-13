3803f0ee72a467d867b98e3c9ed63a3cb6ad156f
Artem Labazov <123321artyom@gmail.com>
2021-04-14 00:09:04 +0300

    Remove unused constructor

--- lab2/src/main/java/AbstractFunction.java
+++ lab2/src/main/java/AbstractFunction.java
@@ -5,14 +5,6 @@ public abstract class AbstractFunction {
     final public Matrix start;
     final public Matrix end;
 
-    AbstractFunction() {
-        A = null;
-        b = null;
-        c = 0;
-        start = null;
-        end = null;
-    }
-
     public AbstractFunction(final Matrix a, final Matrix b, final double c, final Matrix start, final Matrix end) {
         A = a;
         this.b = b;
