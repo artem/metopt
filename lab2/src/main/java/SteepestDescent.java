f7df6d8804536a7303ccb192296002709de6f62c
Ivan Naumov <ivanln7514@gmail.com>
2021-04-13 23:47:18 +0300

    Import single demesion methods from lab1

--- lab2/src/main/java/SteepestDescent.java
+++ lab2/src/main/java/SteepestDescent.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.function.UnaryOperator;
 
 public class SteepestDescent {
     private final AbstractFunction f;
@@ -9,26 +9,6 @@ public class SteepestDescent {
         this.eps = eps;
     }
 
-    private double goldenRatio(final AbstractFunction fun, final double epsLim, final double from, final double to) {
-        final double Tau = (Math.sqrt(5) - 1) / 2;
-        double I = (to - from) * Tau;
-        double[] x = new double[]{to - I, from + I};
-        double[] f = new double[]{
-                fun.eval(new Matrix(List.of(List.of(x[0])))),
-                fun.eval(new Matrix(List.of(List.of(x[1]))))};
-        double[] ab = new double[]{from, to};
-        for (double eps = (ab[1] - ab[0]) / 2; eps > epsLim; eps *= Tau) {
-            I *= Tau;
-            int le = f[0] <= f[1] ? 1 : 0;
-            ab[le] = x[le];
-            x[le] = x[1 - le];
-            x[1 - le] = ab[le] + (le == 0 ? I : -I);
-            f[le] = f[1 - le];
-            f[1 - le] = fun.eval(new Matrix(List.of(List.of(x[1 - le]))));
-        }
-        return (ab[0] + ab[1]) / 2;
-    }
-
     public Matrix process() throws MatrixException {
         final Trace result = new Trace(f);
 
@@ -37,15 +17,10 @@ public class SteepestDescent {
         result.add(x);
 
         while (p.len() > eps) {
-            final Matrix finalX = x;
-            final Matrix finalP = p;
-            final AbstractFunction g = new AbstractFunction() {
-                @Override
-                double eval(Matrix m) throws MatrixException {
-                    return f.eval(Matrix.sum(finalX, Matrix.mul(finalP, m.get(0, 0))));
-                }
-            };
-            final double a = goldenRatio(g, eps, 0, 1000000);
+            Matrix finalX = x;
+            Matrix finalP = p;
+            UnaryOperator<Double> g1 = z -> f.eval(Matrix.sum(finalX, Matrix.mul(finalP, z)));
+            double a = SingleDimensionMethods.parabola(g1, eps, 0, 1000000);
             x = Matrix.sum(x, Matrix.mul(p, a));
             p = f.gradient(x).invert();
             result.add(x);
