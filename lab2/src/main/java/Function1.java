7ddd02895e781e81fe53ace9abf133021f8fff3f
flydzen <flydzen@gmail.com>
2021-04-14 11:42:00 +0300

    Я ж писал что тут бага

--- lab2/src/main/java/Function1.java
+++ lab2/src/main/java/Function1.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class Function1 extends AbstractFunction {
     public Function1() {
         super(
-                new Matrix(List.of(List.of(2 * 54., 126.), List.of(126., 2 * 64.))),
+                new Matrix(List.of(List.of(2 * 64., 126.), List.of(126., 2 * 64.))),
                 new Matrix(List.of(-10., 30.), true),
                 13,
                 new Matrix(List.of(-100., -100.), true),
