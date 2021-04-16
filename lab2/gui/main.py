d62366febcdc484fc5fff9348244f8774e2016f0
flydzen <flydzen@gmail.com>
2021-04-16 21:37:26 +0300

    Add stepping + correct zoom + modal window for plot

--- lab2/gui/main.py
+++ lab2/gui/main.py
@@ -1,15 +1,50 @@
-import random
 import tkinter as tk
 from tkinter import ttk
 import numpy as np
-from pylab import cm, imshow, contour, clabel, colorbar, axis, title, show
+from pylab import cm, imshow, contour, clabel, colorbar, title, show
 
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import values
 import query
 import matplotlib.pyplot as plt
 import matplotlib
+
+
+class SteppingController:
+    def __init__(self, steps: np.ndarray):
+        self.is_active = False
+        self.steps = steps.reshape((steps.shape[0], 2))
+        # self.steps = np.array([(x[0][0], x[1][0]) for x in steps])
+        self.steps_number = self.steps.shape[0]
+        self.current = 0
+
+    def get_radius(self):
+        if self.is_active:
+            x1, y1 = self.steps[self.current]
+            x2, y2 = self.steps[self.current+1]
+            center = (abs(x2-x1), abs(y2-y1))
+            radius = max(abs(x2-x1), (y2-y1))
+            return center, radius*1.2
+        else:
+            x1, y1 = self.steps.min(axis=0)
+            x2, y2 = self.steps.max(axis=0)
+            radius = max(abs(self.steps[-1][0] - x1), abs(self.steps[-1][1] - y1),
+                         abs(self.steps[-1][0] - x2), abs(self.steps[-1][1] - y2))
+            center = self.steps[-1]
+            return center, radius*1.2
+
+    def next(self):
+        if self.is_active:
+            if self.current < self.steps_number-1:
+                self.current += 1
+            return self.current
+
+    def prev(self):
+        if self.is_active:
+            if self.current > 0:
+                self.current -= 1
+            return self.current
 
 
 class GUI:
@@ -20,8 +55,9 @@ class GUI:
 
         self.method = None
         self.functionId = None
-        self.function: values.Function = None
+        self.function = None
         self.steps = None
+        self.steps_controller = None
 
         self.settings = {
             'eps': '1e-3',
@@ -29,8 +65,6 @@ class GUI:
             'alpha': '1',
             'ng': '0'
         }
-
-        self.data = None
 
         matplotlib.use('TkAgg')
         self.window = tk.Tk()
@@ -51,7 +85,7 @@ class GUI:
         func_selector.grid(column=3, row=0, pady=3)
         func_selector.current(0)
 
-        tk.Button(self.window, text='Settings', command=self.func, height=2, width=15, background='black',
+        tk.Button(self.window, text='Parameters', command=self.func, height=2, width=15, background='black',
                   foreground='white').grid(column=0, row=1, pady=5)
         tk.Button(self.window, text='Launch', command=self.launch, height=2, width=15, background='green',
                   foreground='white').grid(column=1, row=1, pady=5)
@@ -68,15 +102,16 @@ class GUI:
         self.zoom_entry = tk.Scale(self.window, from_=1, to=200, len=400, orient=tk.HORIZONTAL, resolution=1)
         self.zoom_entry.grid(column=1, row=2, columnspan=3, pady=3)
 
-        self.fig = plt.figure(1)
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
-        plot_widget = self.canvas.get_tk_widget()
-        plot_widget.grid(column=0, row=4, columnspan=4)
+        # self.fig = plt.figure(1)
+        # self.canvas = FigureCanvasTkAgg(self.fig, master=self.window)
+        self.text_editor = tk.Text()
+        self.text_editor.grid(column=0, row=4, columnspan=4)
 
-        tk.Button(self.window, text='Stepping', command=self.func, height=2, width=15).grid(column=0, row=5, pady=5)
-        tk.Button(self.window, text='Prev', command=self.func, height=2, width=15).grid(column=1, row=5, pady=5)
-        tk.Button(self.window, text='Next', command=self.launch, height=2, width=15).grid(column=2, row=5, pady=5)
-        tk.Button(self.window, text='All', command=self.paint_main_function, height=2, width=15).grid(column=3, row=5, pady=5)
+        tk.Button(self.window, text='Stepping', command=self.steps_activate, height=2, width=15).grid(column=0, row=5, pady=5)
+        tk.Button(self.window, text='Prev', command=self.steps_prev, height=2, width=15).grid(column=1, row=5, pady=5)
+        tk.Button(self.window, text='Next', command=self.steps_next, height=2, width=15).grid(column=2, row=5, pady=5)
+        tk.Button(self.window, text='All', command=self.steps_deactivate, height=2, width=15)\
+            .grid(column=3, row=5, pady=5)
 
         frame_gui_settings = tk.Frame()
         self.var_lines = tk.BooleanVar()
@@ -107,7 +142,7 @@ class GUI:
 
         frame_views = tk.Frame()
         ttk.Label(frame_views, text='Min x:').pack(anchor=tk.W)
-        ttk.Label(frame_views, text='Min y:').pack(anchor=tk.W)
+        ttk.Label(frame_views, text='Min value:').pack(anchor=tk.W)
         ttk.Label(frame_views, text='Steps:').pack(anchor=tk.W)
         frame_views.grid(column=1, row=6)
 
@@ -120,6 +155,26 @@ class GUI:
         self.steps_label.pack(anchor=tk.W)
         frame_res.grid(column=2, row=6, columnspan=2)
 
+    def steps_activate(self):
+        if self.steps_controller:
+            self.steps_controller.is_active = True
+            self.paint_main_function()
+
+    def steps_deactivate(self):
+        if self.steps_controller:
+            self.steps_controller.is_active = False
+            self.paint_main_function()
+
+    def steps_next(self):
+        if self.steps_controller:
+            self.steps_controller.next()
+            self.paint_main_function()
+
+    def steps_prev(self):
+        if self.steps_controller:
+            self.steps_controller.prev()
+            self.paint_main_function()
+
     def launch(self):
         action = values.actions.index(self.current_action.get())
         if action == 0:
@@ -128,10 +183,15 @@ class GUI:
                 if f.description == self.selected_function.get():
                     self.functionId = i
                     break
-            self.function, self.data = query.call(self.method, self.functionId, eps=self.settings['eps'],
-                                                  alpha=self.settings['alpha'], ng=self.settings['ng'])
+            self.function, self.steps = query.call(self.method, self.functionId, eps=self.settings['eps'],
+                                                   alpha=self.settings['alpha'], ng=self.settings['ng'])
             self.function.description = values.functions[self.functionId].description
-            self.center = (self.data[-1]["data"][0][0], self.data[-1]["data"][1][0])
+            self.center = (self.steps[-1][0][0], self.steps[-1][1][0])
+            self.min_x_label['text'] = self.steps[-1].T
+            self.min_y_label['text'] = self.function.eval(self.steps[-1])
+            self.steps_label['text'] = self.steps.shape[0]
+            self.steps_controller = SteppingController(self.steps)
+            self.center, self.size = self.steps_controller.get_radius()
             self.paint_main_function()
 
     def paint_main_function(self):
@@ -144,12 +204,13 @@ class GUI:
         self.radius = self.size/zoom
         x = np.arange(self.center[0] - self.radius, self.center[0] + self.radius, self.radius / 50)
         y = np.arange(self.center[1] - self.radius, self.center[1] + self.radius, self.radius / 50)
-        Z = np.array([[self.function.eval(np.array([j, i]).T)[0] for j in x] for i in y])
-        fg = imshow(Z, cmap=cm.RdBu, extent=(x[0], x[-1], y[-1], y[0]))
-        mini = Z.min()
-        maxi = Z.max()
+        z = np.array([[self.function.eval(np.array([j, i]).T)[0] for j in x] for i in y])
+        fg = imshow(z, cmap=cm.RdBu, extent=(x[0], x[-1], y[-1], y[0]))
+        mini = z.min()
+        maxi = z.max()
         if self.var_lines.get():
-            cset = contour(Z, np.arange(mini, maxi, (maxi-mini)/10), linewidths=2, cmap=cm.Set2, extent=(x[0], x[-1], y[0], y[-1]))
+            cset = contour(z, np.arange(mini, maxi, (maxi-mini)/10), linewidths=2, cmap=cm.Set2,
+                           extent=(x[0], x[-1], y[0], y[-1]))
             clabel(cset, inline=True, fmt='%1.1f', fontsize=10)
         if self.var_text.get():
             title(self.function.description)
@@ -157,26 +218,22 @@ class GUI:
             plt.axis('off')
         else:
             colorbar(fg)
+        start_ind = 0 if not self.steps_controller.is_active else self.steps_controller.current
+        end_ind = self.steps.shape[0] - 1 if not self.steps_controller.is_active else start_ind + 1
         if self.var_arrows.get():
-            for i in range(len(self.data)-1):
-                a1, b1 = np.array(self.data[i]['data']).T[0]
-                a2, b2 = np.array(self.data[i + 1]['data']).T[0]
-                if not self.center[0]-self.radius < a1 < self.center[0]+self.radius:
-                    print("continued")
+            for i in range(start_ind, end_ind):
+                a1, b1 = self.steps[i].T[0]
+                a2, b2 = self.steps[i + 1].T[0]
+                if not self.center[0]-self.radius < a1 < self.center[0]+self.radius or \
+                        not self.center[1]-self.radius < b1 < self.center[1]+self.radius or \
+                        not self.center[0]-self.radius < a2 < self.center[0]+self.radius or \
+                        not self.center[1]-self.radius < b2 < self.center[1]+self.radius:
                     continue
-                if not self.center[1]-self.radius < b1 < self.center[1]+self.radius:
-                    print("continued")
-                    continue
-                if not self.center[0]-self.radius < a2 < self.center[0]+self.radius:
-                    print("continued")
-                    continue
-                if not self.center[1]-self.radius < b2 < self.center[1]+self.radius:
-                    print("continued")
-                    continue
-                plt.arrow(a1, b1, a2-a1-0.01*self.radius, b2-b1-0.01*self.radius, width=0.004*self.radius, head_width=0.05*self.radius,
+                plt.arrow(a1, b1, a2-a1,
+                          b2-b1, width=0.004*self.radius, head_width=0.05*self.radius,
                           head_length=0.01*self.radius, fc='k', ec='k')
         # self.canvas.draw()
-        # show()
+        show()
 
     def func(self):
         def save():
