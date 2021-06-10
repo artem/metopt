1ecee2a173f5930e03ac6b49bcb349f3d33dbcf1
Artem Labazov <123321artyom@gmail.com>
2021-06-11 01:20:47 +0300

    fix fun1

--- lab4/src/main/java/metopt/lab4/functions/Function1.java
+++ lab4/src/main/java/metopt/lab4/functions/Function1.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class Function1 extends QuadraticFunction {
     public Function1() {
         super(new FullMatrix(List.of(
-                new Vector(List.of(1., -0.6)),
-                new Vector(List.of(-0.6, 1.))
+                new Vector(List.of(2., -1.2)),
+                new Vector(List.of(-1.2, 2.))
         )), new Vector(2), 0., new Vector(List.of(4., 1.)), "Function1");
     }
 }
